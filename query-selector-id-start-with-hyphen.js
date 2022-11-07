64749f3679bc35b61086f09d8dde121584bc5e31
coji <coji@techtalk.jp>
2022-11-07 16:54:34 +0900

    comment

--- query-selector-id-start-with-hyphen.js
+++ query-selector-id-start-with-hyphen.js
@@ -8,4 +8,4 @@ const divByGetElementById = document.getElementById("--test--");
 const divByQuerySelector = document.querySelector("#--test--");
 
 console.log(divByGetElementById?.outerHTML ?? "Not found"); // <div id="--test--">test</div>
-console.log(divByQuerySelector?.outerHTML ?? "Not found"); // <div id="--test--">test</div>
+console.log(divByQuerySelector?.outerHTML ?? "Not found"); // Not found *bug*
