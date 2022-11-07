53534182403dba1d49963b36f29ac9844f978760
coji <coji@techtalk.jp>
2022-11-07 16:51:58 +0900

    init

---
+++ query-selector-id-start-with-hyphen.js
@@ -0,0 +1,11 @@
+const { Window } = require("happy-dom");
+
+const window = new Window();
+const document = window.document;
+
+document.body.innerHTML = '<div id="--test--">test!</div>';
+const divByGetElementById = document.getElementById("--test--");
+const divByQuerySelector = document.querySelector("#--test--");
+
+console.log(divByGetElementById?.outerHTML ?? "Not found"); // <div id="--test--">test</div>
+console.log(divByQuerySelector?.outerHTML ?? "Not found"); // <div id="--test--">test</div>
